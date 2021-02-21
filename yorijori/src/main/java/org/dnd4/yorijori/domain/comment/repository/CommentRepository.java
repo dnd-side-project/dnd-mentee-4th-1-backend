aa6cbd0426589bbf4a665f08af3e31a4d12ed293
soob-forest <dltntnqo@naver.com>
2021-02-21 19:16:50 +0900

    [ADD] comment repository에 recipeId로 find 함수

--- yorijori/src/main/java/org/dnd4/yorijori/domain/comment/repository/CommentRepository.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/comment/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package org.dnd4.yorijori.domain.comment.repository;
 import org.dnd4.yorijori.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByRecipeId(Long recipeId);
 }
