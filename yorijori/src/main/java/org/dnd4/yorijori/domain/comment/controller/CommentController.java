2611b364c7fd230b36aa76e013523fb14d7c7fbf
soob-forest <dltntnqo@naver.com>
2021-02-21 21:07:42 +0900

    [ADD] comment add 서비스

--- yorijori/src/main/java/org/dnd4/yorijori/domain/comment/controller/CommentController.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/comment/controller/CommentController.java
@@ -1,20 +1,41 @@
 package org.dnd4.yorijori.domain.comment.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.dnd4.yorijori.domain.comment.dto.RequestCommentDto;
 import org.dnd4.yorijori.domain.comment.dto.ResponseCommentDto;
+import org.dnd4.yorijori.domain.comment.entity.Comment;
 import org.dnd4.yorijori.domain.comment.service.CommentService;
 import org.dnd4.yorijori.domain.common.Result;
 import org.dnd4.yorijori.domain.common.ResultList;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.dnd4.yorijori.domain.recipe.dto.ResponseDto;
+import org.dnd4.yorijori.domain.recipe.dto.UpdateRequestDto;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-
+@RequestMapping("/comments")
 public class CommentController {
     private final CommentService commentService;
 
+//    @GetMapping("/{id}")
+//
+//    @PutMapping("/{id}")
+//    public Result<ResponseDto> update (@PathVariable Long id, @RequestBody @Validated UpdateRequestDto reqDto){
+//
+//
+//    }
+
+    @PostMapping("")
+    public Result<ResponseCommentDto> add(@RequestBody @Validated RequestCommentDto commentDto){
+
+        Long id = commentService.add(commentDto);
+        Comment comment = commentService.get(id);
+        return new Result<ResponseCommentDto>(new ResponseCommentDto(comment));
+
+    }
+//
+//    @DeleteMapping("/{id}")
 
 
 }
--- yorijori/src/main/java/org/dnd4/yorijori/domain/comment/service/CommentService.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/comment/service/CommentService.java
@@ -1,9 +1,14 @@
 package org.dnd4.yorijori.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
+import org.dnd4.yorijori.domain.comment.dto.RequestCommentDto;
 import org.dnd4.yorijori.domain.comment.dto.ResponseCommentDto;
 import org.dnd4.yorijori.domain.comment.entity.Comment;
 import org.dnd4.yorijori.domain.comment.repository.CommentRepository;
+import org.dnd4.yorijori.domain.recipe.entity.Recipe;
+import org.dnd4.yorijori.domain.recipe.repository.RecipeRepository;
+import org.dnd4.yorijori.domain.user.entity.User;
+import org.dnd4.yorijori.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,10 +20,40 @@ import java.util.List;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final RecipeRepository recipeRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public List<Comment> findByRecipeId(Long recipeId){
 
         return commentRepository.findByRecipeId(recipeId);
+    }
+
+    @Transactional
+    public Long add(RequestCommentDto commentDto){
+
+        Recipe recipe = recipeRepository.findById(commentDto.getRecipeId()).orElseThrow(()->new IllegalArgumentException("해당 아이디의 레시피가 없습니다. id : " + commentDto.getRecipeId()));
+        User user = userRepository.findById(commentDto.getUserId()).orElseThrow(()->new IllegalArgumentException("해당 아이디의 유저가 없습니다. id : " + commentDto.getUserId()));
+
+        Comment parentComment = null;
+        if(commentDto.getPid() != null) parentComment = commentRepository.findById(commentDto.getPid()).orElseThrow(()->new IllegalArgumentException("해당 부모 아이디의 댓글이 없습니다. id : " + commentDto.getPid()));
+
+        Comment comment = Comment.builder()
+                .content(commentDto.getContent())
+                .imageUrl(commentDto.getImageUrl())
+                .parent(parentComment)
+                .user(user)
+                .recipe(recipe)
+                .build();
+
+        return commentRepository.save(comment).getId();
+
+    }
+
+    @Transactional
+    public Comment get(Long id){
+        Comment comment = commentRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 아이디의 댓글이 없습니다. id : " + id));
+
+        return comment;
     }
 }
