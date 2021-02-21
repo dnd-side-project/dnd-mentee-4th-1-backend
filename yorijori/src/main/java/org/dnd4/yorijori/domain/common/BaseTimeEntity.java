d9045c604fb710867d2caaabce8c3cc456767f5d
soob-forest <dltntnqo@naver.com>
2021-02-21 21:06:54 +0900

    [FIX] baeTimeEntity setter 추가

--- yorijori/src/main/java/org/dnd4/yorijori/domain/common/BaseTimeEntity.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/common/BaseTimeEntity.java
@@ -5,13 +5,15 @@ import java.time.LocalDateTime;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
 
-@Getter
+@Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
