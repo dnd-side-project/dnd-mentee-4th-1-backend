37d46fd612ec3abce13aee2d123595a305679a73
soob-forest <dltntnqo@naver.com>
2021-02-21 21:10:21 +0900

    [ADD] comment 하나 가져오는 controller

--- yorijori/src/main/java/org/dnd4/yorijori/domain/comment/controller/CommentController.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/comment/controller/CommentController.java
@@ -18,7 +18,11 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     private final CommentService commentService;
 
-//    @GetMapping("/{id}")
+    @GetMapping("/{id}")
+    public Result<ResponseCommentDto> get(@PathVariable Long id){
+        Comment comment = commentService.get(id);
+        return new Result<ResponseCommentDto>(new ResponseCommentDto(comment));
+    }
 //
 //    @PutMapping("/{id}")
 //    public Result<ResponseDto> update (@PathVariable Long id, @RequestBody @Validated UpdateRequestDto reqDto){
