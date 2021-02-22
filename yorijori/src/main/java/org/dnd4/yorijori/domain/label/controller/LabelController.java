8c2309c97c1806c57071cdc79a91c81b634f1fe4
홍지영 <0___0as@naver.com>
2021-02-23 03:01:33 +0900

    [Add] 사용자 찜 리스트

--- yorijori/src/main/java/org/dnd4/yorijori/domain/label/controller/LabelController.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/label/controller/LabelController.java
@@ -3,29 +3,35 @@ package org.dnd4.yorijori.domain.label.controller;
 import java.security.Principal;
 
 import org.dnd4.yorijori.domain.common.Result;
-import org.dnd4.yorijori.domain.label.entity.Label;
+import org.dnd4.yorijori.domain.common.ResultList;
 import org.dnd4.yorijori.domain.label.service.LabelService;
+import org.dnd4.yorijori.domain.recipe.dto.ResponseDto;
+import org.dnd4.yorijori.domain.recipe.dto.UserDto;
 import org.dnd4.yorijori.domain.recipe.entity.Recipe;
 import org.dnd4.yorijori.domain.recipe.service.RecipeService;
 import org.dnd4.yorijori.domain.user.entity.User;
+import org.dnd4.yorijori.domain.user.repository.UserRepository;
+import org.dnd4.yorijori.domain.user_follow.service.UserFollowService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/recipes")
 public class LabelController {
 
 	private final RecipeService recipeService;
 	private final LabelService labelService;
 
-	@PostMapping("/{id}/label")
+	@PostMapping("/recipes/{id}/label")
 	public Result<Boolean> add(@PathVariable Long id, Principal principal) {
 		Recipe recipe = recipeService.get(id);
 		User user = (User) ((Authentication) principal).getPrincipal();
@@ -33,12 +39,24 @@ public class LabelController {
 		return new Result<Boolean>(true);
 	}
 
-	@DeleteMapping("/{id}/label")
+	@DeleteMapping("/recipes/{id}/label")
 	public Result<Boolean> delete(@PathVariable Long id, Principal principal) {
 		Recipe recipe = recipeService.get(id);
 		User user = (User) ((Authentication) principal).getPrincipal();
 		labelService.delete(user, recipe);
 		return new Result<Boolean>(true);
+	}
+
+	private final UserRepository userRepository;
+
+	@GetMapping("/user/{userId}/label")
+	@PreAuthorize("#userId==principal.id")
+	public ResultList<ResponseDto> labelList(@PathVariable Long userId,
+			@RequestParam(required = false, defaultValue = "10") int limit,
+			@RequestParam(required = false, defaultValue = "0") int offset) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new IllegalArgumentException("해당 아이디의 유저가 없습니다. id : " + userId));
+		return new ResultList<ResponseDto>(labelService.labelList(user, limit, offset));
 	}
 
 }
--- yorijori/src/main/java/org/dnd4/yorijori/domain/label/service/LabelService.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/label/service/LabelService.java
@@ -1,7 +1,12 @@
 package org.dnd4.yorijori.domain.label.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.dnd4.yorijori.domain.label.entity.Label;
+import org.dnd4.yorijori.domain.label.repository.LabelDslRepository;
 import org.dnd4.yorijori.domain.label.repository.LabelRepository;
+import org.dnd4.yorijori.domain.recipe.dto.ResponseDto;
 import org.dnd4.yorijori.domain.recipe.entity.Recipe;
 import org.dnd4.yorijori.domain.user.entity.User;
 import org.springframework.stereotype.Service;
@@ -14,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class LabelService {
 
 	private final LabelRepository labelRepository;
+	private final LabelDslRepository labelDslRepository;
 
 	@Transactional
 	public void add(User user, Recipe recipe) {
@@ -25,5 +31,10 @@ public class LabelService {
 	public void delete(User user, Recipe recipe) {
 		Label entity = labelRepository.findByUserIdAndRecipeId(user.getId(), recipe.getId());
 		labelRepository.delete(entity);
+	}
+
+	public List<ResponseDto> labelList(User user, int limit, int offset) {
+		return labelDslRepository.labelList(user, limit, offset).stream().map(ResponseDto::new)
+				.collect(Collectors.toList());
 	}
 }
