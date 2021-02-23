df12b8fa14213298ca44f4b62b02975bfae51312
홍지영 <0___0as@naver.com>
2021-02-23 19:00:44 +0900

    [Fix] 컨트롤러에서 순서변경

--- yorijori/src/main/java/org/dnd4/yorijori/domain/label/controller/LabelController.java
+++ yorijori/src/main/java/org/dnd4/yorijori/domain/label/controller/LabelController.java
@@ -4,21 +4,19 @@ import java.security.Principal;
 
 import org.dnd4.yorijori.domain.common.Result;
 import org.dnd4.yorijori.domain.common.ResultList;
+import org.dnd4.yorijori.domain.label.repository.LabelRepository;
 import org.dnd4.yorijori.domain.label.service.LabelService;
 import org.dnd4.yorijori.domain.recipe.dto.ResponseDto;
-import org.dnd4.yorijori.domain.recipe.dto.UserDto;
 import org.dnd4.yorijori.domain.recipe.entity.Recipe;
 import org.dnd4.yorijori.domain.recipe.service.RecipeService;
 import org.dnd4.yorijori.domain.user.entity.User;
 import org.dnd4.yorijori.domain.user.repository.UserRepository;
-import org.dnd4.yorijori.domain.user_follow.service.UserFollowService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,18 +32,18 @@ public class LabelController {
 	@PostMapping("/recipes/{id}/label")
 	public Result<Boolean> add(@PathVariable Long id, Principal principal) {
 		Recipe recipe = recipeService.get(id);
-		recipeService.incWishCount(recipe);
 		User user = (User) ((Authentication) principal).getPrincipal();
 		labelService.add(user, recipe);
+		recipeService.incWishCount(recipe);
 		return new Result<Boolean>(true);
 	}
 
 	@DeleteMapping("/recipes/{id}/label")
 	public Result<Boolean> delete(@PathVariable Long id, Principal principal) {
 		Recipe recipe = recipeService.get(id);
-		recipeService.decWishCount(recipe);
-		User user = (User) ((Authentication) principal).getPrincipal();
+		User user = (User) ((Authentication) principal).getPrincipal();		
 		labelService.delete(user, recipe);
+		recipeService.decWishCount(recipe);
 		return new Result<Boolean>(true);
 	}
 
